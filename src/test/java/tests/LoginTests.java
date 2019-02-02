6434a4056c36d67102dc849054312667d93c483a
CybertekSchool <github@cybertekschool.com>
2019-02-01 22:16:58 -0500

    commit 4. Add negativeLogInTest

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -34,6 +34,17 @@ public class LoginTests {
     }
 
     @Test
+    public void negativeloginTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester2");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test2" + Keys.ENTER);
+        String errorMsg = driver.findElement(By.id("ctl00_MainContent_status")).getText();
+
+        Assert.assertEquals(errorMsg, "Invalid Login or Password.");
+    }
+
+
+    @Test
     public void logOutTest() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
